c7f3d693bdba6ee6efc8b74081c19992ba8c212d
Walter Nuccio <walter.nuccio@edreamsodigeo.com>
2025-07-09 08:07:01 +0200

    Change color on mouse enter

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type ReactTitleProps = {
+  color: string;
+};
+function ReactTitle({ color }: ReactTitleProps, color2: string = 'red') {
+  return (
+    <span>
+      This is my first{' '}
+      <span
+        style={{ color }}
+        onMouseEnter={e => (e.currentTarget.style.color = 'yellow')}
+        onMouseLeave={e => (e.currentTarget.style.color = color)}
+      >
+        {' React'}
+      </span>{' '}
+      app
+    </span>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <ReactTitle color={"green"} />
+          <br />
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
